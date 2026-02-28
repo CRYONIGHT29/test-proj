dca2f87c1954f77a61e8878fbbfeae5774e0434d
CRYONIGHT29 <94116805+CRYONIGHT29@users.noreply.github.com>
2026-02-28 11:42:04 +0530

    added print function

--- 9_NQueens/main.c
+++ 9_NQueens/main.c
@@ -4,6 +4,7 @@
 int n;
 int count = 0;
 int board[20];
+int printed = 0;   // NEW: to print only first solution
 
 int safe(int row, int col)
 {
@@ -17,13 +18,39 @@ int safe(int row, int col)
     return 1;
 }
 
+void printBoard()
+{
+    for(int r=0; r<n; r++)
+    {
+        for(int c=0; c<n; c++)
+        {
+            if(board[r] == c)
+                printf("Q ");
+            else
+                printf(". ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
 void solve(int row)
 {
     if (row==n)
     {
         count++;
+
+        // Print only first solution
+        if (printed == 0)
+        {
+            printBoard();
+            printed = 1;
+        }
+
         return;
     }
+
     for(int col=0; col<n; col++)
     {
         if (safe(row, col))
@@ -38,8 +65,10 @@ int main()
 {
     printf("Enter the number of rows\n");
     scanf("%d", &n);
+
     solve(0);
-    printf("Total solutions are %d", count);
+
+    printf("Total solutions are %d\n", count);
+
     return 0;
-    
 }
